9755a474077b3ae40d7027d5da50b99778626019
Lucas <lucas.dantas.nogueira@gmail.com>
2023-10-31 10:59:35 -0300

    :recycle: refactor: retirada de items comentados

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -41,17 +41,6 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
         'items' => [
             ['label' => 'Pessoas', 'url' => ['/site/index']],
             ['label' => 'Atributos', 'url' => ['/site/about']],
-            // ['label' => 'Contact', 'url' => ['/site/contact']],
-            // Yii::$app->user->isGuest
-            //     ? ['label' => 'Login', 'url' => ['/site/login']]
-            //     : '<li class="nav-item">'
-            //         . Html::beginForm(['/site/logout'])
-            //         . Html::submitButton(
-            //             'Logout (' . Yii::$app->user->identity->username . ')',
-            //             ['class' => 'nav-link btn btn-link logout']
-            //         )
-            //         . Html::endForm()
-            //         . '</li>'
         ]
     ]);
     NavBar::end();
